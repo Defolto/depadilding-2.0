57aaef7404b0b5d1521c51990b31625dbf1f6105
Defolto <37343435+Defolto@users.noreply.github.com>
2020-02-06 15:32:16 +0300

    add page sendvich-panel

--- material/script.js
+++ material/script.js
@@ -49,3 +49,16 @@ $('.camburger').click(function () {
         cambFlag = true;
     }
 });
+
+$(".selection:nth-child(1)").click(function () {
+    $(".picture").css({"background": "url(img/komer.jpg)", "background-size": "cover", "height": "70vh"});
+});
+$(".selection:nth-child(2)").click(function () {
+    $(".picture").css({"background": "url(img/sklad.jpg)", "background-size": "cover", "height": "70vh"});
+});
+$(".selection:nth-child(3)").click(function () {
+    $(".picture").css({"background": "url(img/logich.jpg)", "background-size": "cover", "height": "70vh"});
+});
+$(".selection:nth-child(4)").click(function () {
+    $(".picture").css({"background": "url(img/jkx.jpg)", "background-size": "cover", "height": "70vh"});
+});
